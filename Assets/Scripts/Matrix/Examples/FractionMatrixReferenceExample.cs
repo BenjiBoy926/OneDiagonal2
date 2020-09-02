f16202a3348f82e004b2d449460a06af0dd9dd4e
Codey Huntting <creativecodey@gmail.com>
2020-09-01 17:08:37 -0700

    Added fraction matrix reference and variables

---
+++ Assets/Scripts/Matrix/Examples/FractionMatrixReferenceExample.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public class FractionMatrixReferenceExample : MonoBehaviour
+{
+    [SerializeField]
+    private FractionMatrixReference reference;
+}
---
+++ Assets/Scripts/Matrix/Graphing/FractionMatrixReference.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class FractionMatrixReference : Reference<FractionMatrix>
+{
+    [SerializeField]
+    [Tooltip("Type of reference")]
+    private ReferenceType type;
+
+    [SerializeField]
+    [Tooltip("Component used to get the int, in the case of a variable reference type")]
+    private FractionMatrixVariable component = null;
+
+    [SerializeField]
+    [Tooltip("Value in the reference")]
+    private FractionMatrix value;
+
+    public override FractionMatrix GetValue()
+    {
+        switch (type)
+        {
+            case ReferenceType.Value: return value;
+            case ReferenceType.Reference: return component.GetValue();
+            default: return value;
+        }
+    }
+
+    public override Variable<FractionMatrix> GetReference()
+    {
+        return component;
+    }
+}
