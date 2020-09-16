00a3e3bf51c6b8f0629a63fb327025133c56e069
Codey Huntting <creativecodey@gmail.com>
2020-09-16 15:54:28 -0700

    Matrix text glows green when in the proper position

---
+++ Assets/Scripts/GraphingExtension/Actions/GetTimerFormattedString.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using System;
+
+public class GetTimerFormattedString : MonoBehaviour
+{
+    public Input<float> timer;
+
+    public Result<string> result;
+
+    public UnityEvent output;
+
+    public void Invoke()
+    {
+        TimeSpan time = TimeSpan.FromSeconds(timer.value);
+        result.value = time.ToString("mm\\:ss\\.fff");
+        output.Invoke();
+    }
+}
